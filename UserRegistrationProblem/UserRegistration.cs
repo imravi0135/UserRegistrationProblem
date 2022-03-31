f799b21fc9cd384dd18221e154bc1766947ccc4e
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-31 14:38:26 +0530

    Welcome to User Registration Problems

---
+++ UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UserRegistration
+    {
+        public Regex FirstName = new Regex(@"^[A-Z][A-Za-z]{2,}$");
+        public void ValidFirstName(string firstName)
+        {
+            if (FirstName.IsMatch(firstName))
+                Console.WriteLine("firstName is valid");
+            else
+                Console.WriteLine("firstName is Invalid");
+
+        }
+    }
+}
